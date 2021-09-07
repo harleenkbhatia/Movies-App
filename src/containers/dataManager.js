490b0a036a7d4769427320d012e18c2bf58c78d1
harleenkbhatia <74737625+harleenkbhatia@users.noreply.github.com>
2021-09-07 14:58:50 +0530

    Delete src directory

--- src/containers/dataManager.js
+++
@@ -1,6 +0,0 @@
-import { GetService } from "../../services/movies";
-
-export const HandleGetMovies = async () => {
-    const result = await GetService();
-    return result;
-}
--- src/services/movies.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const GetService = async () => {
-    const response = await axios.get("https://pephellomivies.free.beeceptor.com/movies");
-    return response.data
-}
